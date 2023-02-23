7ba8255450f650e2273ac8ba78011994af34faad
Rodrigo Docinel <docinel@gmail.com>
2023-02-23 20:57:14 -0300

    Alteracao da IDE para Spring Boot

---
+++ os/src/main/java/com/docinel/os/domain/Cliente.java
@@ -0,0 +1,15 @@
+package com.docinel.os.domain;
+
+public class Cliente extends Pessoa{
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(Integer id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+	}
+	
+	
+
+}
