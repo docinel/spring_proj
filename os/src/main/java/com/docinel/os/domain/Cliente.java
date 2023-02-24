627a93698243afe35e469c1818e7769d826e723f
docinel <docinel@gmail.com>
2023-02-24 16:04:21 -0300

    Restruturacao do código

---
+++ os/src/main/java/com/docinel/os/domain/Cliente.java
@@ -0,0 +1,27 @@
+package com.docinel.os.domain;
+
+public class Cliente extends Pessoa implements Serializable{
+    private static final long serialVersionUID = 1L; 
+    
+    @OneToMany(mappedBy = "cliente")
+    private List<OS> list = new ArrayList<>();
+
+    public Cliente() {
+        super();
+    }
+
+    public Cliente(Integer id, String nome, String cpf, String telefone) {
+        super(id, nome, cpf, telefone);
+    }
+
+    public List<OS> getList() {
+        return list;
+    }
+
+    public void setList(List<OS> list) {
+        this.list = list;
+    }
+    
+    
+
+}
